e67db7b653c133924d85ce77e250b48164f5852f
Mark Fabrizio <fabrizim@owlwatch.com>
2024-12-06 11:38:31 -0500

    defer the marketo form classes

--- wp-content/themes/aras/external/storylane.js
+++ wp-content/themes/aras/external/storylane.js
@@ -69,12 +69,14 @@ function arasInitDemandBase(){
 		});
 	}
 
-	// clean up the marketo form so we can style it...
-	const mkto_fields = document.querySelectorAll('.mktoField');
-	mkto_fields.forEach( mkto_field => {
-		const col = mkto_field.closest('.mktoFormCol');
-		if( col && col.tagName && col.tagName == 'DIV' && mkto_field.getAttribute('type')){
-			col.classList.add('type_'+ mkto_field.getAttribute('type') );
-		}
-	});
+	setTimeout( () => {
+		// clean up the marketo form so we can style it...
+		const mkto_fields = document.querySelectorAll('.mktoField');
+		mkto_fields.forEach( mkto_field => {
+			const col = mkto_field.closest('.mktoFormCol');
+			if( col && col.tagName && col.tagName == 'DIV' && mkto_field.getAttribute('type')){
+				col.classList.add('type_'+ mkto_field.getAttribute('type') );
+			}
+		});
+	}, 100 );
 }
