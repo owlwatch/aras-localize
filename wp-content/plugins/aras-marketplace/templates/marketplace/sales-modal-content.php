6602fed5f4f27efefe9d2009c12bd45d3dec18c2
Mark Fabrizio <fabrizim@owlwatch.com>
2025-03-18 14:04:33 -0400

    add text replacements

--- wp-content/plugins/aras-marketplace/templates/marketplace/sales-modal-content.php
+++ wp-content/plugins/aras-marketplace/templates/marketplace/sales-modal-content.php
@@ -22,9 +22,43 @@ $fields[] = [
 	'post_id' => 'option'
 ];
 
+function sales_modal_replacements($content){
+	
+	$contributor = get_first_term('mp-contributor', get_the_ID() );
+	$content = preg_replace_callback('/{{\s*?(.+)\.(.+)\s*?}}/', function($matches) use ($contributor){
+		$object = $matches[1];
+		$var = $matches[2];
+		$value = '';
+		switch( $object ){
+			case 'contributor':
+				switch($var){
+					case 'name':
+						$value = $contributor->name;
+						break;
+					case 'email':
+						$value = get_field('email', $contributor);
+						break;
+
+				}
+				break;
+			case 'solution':
+				switch($var){
+					case 'name':
+						$value = get_the_title();
+						break;
+				}
+				break;
+		}
+		return $value;
+	}, $content);
+	
+	return $content;
+}
+
 foreach( $fields as $field ){
 	$v = get_field( $field['field_name'], $field['post_id'] );
 	if( $v && !empty( $v['flexible_post_content'] )){
+		add_filter('acf_the_content', 'Aras\Marketplace\sales_modal_replacements', 10, 1);
 		?>
 		<div class="reveal medium" id="aras-solution-modal" data-reveal>
 			<?php
@@ -41,6 +75,7 @@ foreach( $fields as $field ){
 			<?php _e('Get it', 'aras-marketplace'); ?>
 			</button>
 		<?php
+		remove_filter('acf_the_content', 'Aras\Marketplace\sales_modal_replacements', 10);
 		break;
 	}
 }
