24c16a1b94c73799946d91955462bfb5bdcccf03
Mark Fabrizio <fabrizim@owlwatch.com>
2025-03-19 21:25:54 -0400

    add upd_email dataLayer variable

---
+++ wp-content/themes/aras/functions/google-ads-enhanced-conversions.php
@@ -0,0 +1,52 @@
+<?php
+namespace Aras;
+
+
+class GoogleAdsEnhancedConversions
+{
+	private static $instance;
+
+	public static function getInstance()
+	{
+		if (! isset(self::$instance)) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	private function __construct()
+	{
+		add_filter('gform_confirmation', [$this, 'gform_confirmation'], 21, 4);
+	}
+
+	public function gform_confirmation($confirmation, $form, $entry, $ajax)
+	{
+		// we are going to assume that the confirmation is a string
+		if( !is_string($confirmation) ){
+			return $confirmation;
+		}
+
+		// find the email from the form
+		$email = '';
+		foreach( $form['fields'] as $field ){
+			if( $field->type === 'email' && $field->required ){
+				$email = rgar($entry, $field->id);
+				break;
+			}
+		}
+
+		if( !$email ){
+			return $confirmation;
+		}
+
+		$script = <<<EOC
+<script type="text/javascript">
+if( window.dataLayer ) dataLayer.push({ 'upd_email' : '{$email}'});
+</script>
+EOC;
+		$confirmation = $script . $confirmation;
+		return $confirmation;
+
+	}
+}
