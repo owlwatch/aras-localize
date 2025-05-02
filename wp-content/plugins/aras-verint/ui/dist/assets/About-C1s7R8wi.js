9b7c3ab6ca30b3b7a2b2dea0e40848499ddac3c6
Mark Fabrizio <fabrizim@owlwatch.com>
2025-05-02 17:16:00 -0400

    build the current ui

---
+++ wp-content/plugins/aras-verint/ui/dist/assets/About-C1s7R8wi.js
@@ -0,0 +1,1 @@
+import{c as e,o as n,_ as c}from"./main-wzvpCUmG.js";function o(r,a){return n(),e("p",null,"Simple UI for verint cleanup functions")}const t={},l=c(t,[["render",o]]);export{l as default};
