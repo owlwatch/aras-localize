ed69ce816ff64dc7a0b0aacf031c8af137f0dadc
Mark Fabrizio <fabrizim@owlwatch.com>
2025-06-17 07:00:55 -0400

    Fix password protection

--- wp-content/themes/aras/parts/_flexible_content/_flexible_content.php
+++ wp-content/themes/aras/parts/_flexible_content/_flexible_content.php
@@ -1,8 +1,40 @@
 <?php
 
-// if this is password protected, just show the_content
+// if the post is password protected and they have not entered the password, return early
 if (post_password_required()) {
-  the_content();
+?>
+  <section class="content-section mediumtoppadding smallbottompadding bg-white " id="contentsection-1">
+
+
+    <div class="grid-container">
+      <div class="grid-x grid-margin-x align-top" data-equalizer="" data-equalizer-by-row="true" data-resize="uwu7ga-eq" data-mutate="b0xc1d-eq" data-t="woswv5-t" data-events="resize">
+
+
+
+        <div class="cell text-block wysiwyg-content small-12 fullwidthblock">
+          <?php
+          $the_content = get_the_content();
+          // update the input[type="submit"] button to add a class .button
+          $the_content = preg_replace('/<input type="submit"/', '<input type="submit" class="aras-button"', $the_content);
+          echo apply_filters('the_content', $the_content);
+          ?>
+        </div>
+
+        <style>
+          form.post-password-form {
+            max-width: 400px;
+            margin: 0 auto;
+
+            input[type="password"] {
+              width: 100%;
+            }
+          }
+        </style>
+
+      </div>
+    </div>
+  </section>
+<?php
   return;
 }
 
@@ -60,7 +92,7 @@ $current_post_id = get_the_ID(); ?>
 
       // try to get the associated template part
       get_template_part('parts/_flexible_content/' . get_row_layout());
-      
+
       ?>
 
     <?php endwhile; ?>
