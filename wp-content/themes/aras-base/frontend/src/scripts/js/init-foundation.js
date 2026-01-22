4ad18906d8b40c1236a476a2911e80b4f467604d
Mark Fabrizio <fabrizim@owlwatch.com>
2026-01-22 18:10:20 -0500

    Enhance scroll behavior handling in Foundation Reveal to prevent jumping

--- wp-content/themes/aras-base/frontend/src/scripts/js/init-foundation.js
+++ wp-content/themes/aras-base/frontend/src/scripts/js/init-foundation.js
@@ -1,2 +1,5 @@
 import { jQuery } from 'jquery';
 jQuery(document).foundation();
+
+// lets fix some of the issues with foundation
+console.log( window.Reveal );
--- wp-content/themes/aras/frontend/src/js/init-foundation.js
+++ wp-content/themes/aras/frontend/src/js/init-foundation.js
@@ -1,2 +1,20 @@
 
 jQuery(document).foundation();
+
+try {
+	const enableScroll = window.Foundation.Reveal.prototype._enableScroll;
+	const disableScroll = window.Foundation.Reveal.prototype._disableScroll;
+
+	window.Foundation.Reveal.prototype._enableScroll = function() {
+		// set the scroll behavior to auto to prevent jumping
+		jQuery('html').css('scroll-behavior', 'auto');
+		enableScroll.apply(this, arguments);
+		// wait a tick and restore the scroll behavior
+		setTimeout(() => {
+			jQuery('html').css('scroll-behavior', '');
+		}, 1);
+		
+	};
+}catch (e) {
+	console.warn('Foundation Reveal not found on window.Foundation');
+}
