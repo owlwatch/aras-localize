b4158faf10bb299ddc3b442bbbcf573d602882b3
Mark Fabrizio <fabrizim@owlwatch.com>
2026-01-22 13:06:50 -0500

    Fix form size when in right column

--- wp-content/themes/aras-base/parts/_flexible_content/split_content_section.php
+++ wp-content/themes/aras-base/parts/_flexible_content/split_content_section.php
@@ -309,7 +309,7 @@
 
 
 							<?php if (have_rows('form_block')) : ?>
-								<div class="cell form-block small-12 medium-order-1 <?= "$leftsize $leftmobile" ?>">
+								<div class="cell form-block small-12 medium-order-1 <?= "$rightsize $rightmobile" ?>">
 									<?php while (have_rows('form_block')) : the_row(); ?>
 
 										<div id="" class="hero-form-container form-left">
