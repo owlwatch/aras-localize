c69266a01ed686718f81e2127d308d1aefec8ee9
Mark Fabrizio <fabrizim@owlwatch.com>
2026-05-07 13:14:39 -0400

    Refactor code structure for improved readability and maintainability

--- wp-content/themes/aras-base/functions/scripts/index.php
+++ wp-content/themes/aras-base/functions/scripts/index.php
@@ -14,6 +14,7 @@ add_action('init', function(){
 	switch( $_REQUEST['aras_script'] ){
 		case 'export_resources':
 			aras_export_resources();
+			break;
 
 		case 'find_posts_with_quote':
 			aras_export_posts_with_quote();
@@ -44,43 +45,74 @@ function aras_export_resources()
 	]);
 
 	fputcsv( $out, [
-		'ID',
-		'Title',
-		'Permalink',
-		'Edit Link',
-		'Date',
-		'Last Modified',
-		'File',
-		'Formats',
-		'Topics'
+		'title',
+		'permalink',
+		'language',
+		'salesforce campaign',
+		'file',
+		'special resource',
+		'publish date',
+		'page template',
+		'format',
+		'applications',
+		'topics',
+		'industries'
 	]);
 	
 	while( $resource_query->have_posts() ){
 		$resource_query->the_post();
 		$button = get_field('post_submission_button');
+		$post_id = get_the_ID();
 
-		$formats = get_the_terms( get_the_ID(), 'format' );
+		$formats = get_the_terms( $post_id, 'format' );
 		$all_formats = is_array( $formats ) ? implode(', ', array_map(function($term){
 			return $term->name;
 		}, $formats)) : '';
 
-		$topics = get_the_terms( get_the_ID(), 'topic' );
+		$applications = get_the_terms( $post_id, 'application' );
+		$all_applications = is_array($applications) ? implode(', ', array_map(function($term){
+			return $term->name;
+		}, $applications)) : '';
+
+		$topics = get_the_terms( $post_id, 'topic' );
 		$all_topics = is_array($topics) ? implode(', ', array_map(function($term){
 			return $term->name;
 		}, $topics)) : '';
 
+		$industries = get_the_terms( $post_id, 'industry' );
+		$all_industries = is_array($industries) ? implode(', ', array_map(function($term){
+			return $term->name;
+		}, $industries)) : '';
+
+		$special_resource_terms = get_the_terms( $post_id, 'featured-resource' );
+		$all_special_resources = is_array($special_resource_terms) ? implode(', ', array_map(function($term){
+			return $term->name;
+		}, $special_resource_terms)) : '';
+
+		$language = '';
+		$wpml_language_details = apply_filters('wpml_post_language_details', null, $post_id);
+		if( is_array($wpml_language_details) && !empty($wpml_language_details['language_code']) ){
+			$language = $wpml_language_details['language_code'];
+		}
+		elseif( !empty($_COOKIE['wp-wpml_current_language']) ){
+			$language = sanitize_text_field($_COOKIE['wp-wpml_current_language']);
+		}
+
 		
 		
 		fputcsv( $out, [
-			get_the_ID(),
 			get_the_title(),
 			get_permalink(),
-			get_edit_post_link(),
-			get_the_date('Y-m-d H:i:s'),
-			get_the_modified_date('Y-m-d H:i:s'),
+			$language,
+			get_field('salesforce_campaign', $post_id),
 			$button ? $button['url'] : 'no file',
+			$all_special_resources,
+			get_the_date('Y-m-d H:i:s'),
+			get_page_template_slug( $post_id ),
 			$all_formats,
-			$all_topics
+			$all_applications,
+			$all_topics,
+			$all_industries
 		]);
 	}
 	fclose($out);
