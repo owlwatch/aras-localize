83e762cef30be9d4cf078a88ec69aa2282e16d7a
Mark Fabrizio <fabrizim@owlwatch.com>
2026-06-12 12:05:18 -0400

    feat: add ACF fields for hreflang settings and enable option

--- lib/Module/Hreflang.php
+++ lib/Module/Hreflang.php
@@ -4,9 +4,29 @@ namespace Aras\Localize\Module;
 use Aras\Localize\Util\Common;
 
 class Hreflang {
+    const FIELD_ENABLE_HREFLANG = 'enable_hreflang';
 
     public function register() {
         add_action('wp_head', [$this, 'render'], 5);
+    }
+
+    public function get_acf_fields() {
+        return [
+            [
+                'key' => 'field_aras_localize_hreflang_tab',
+                'label' => 'Hreflang',
+                'type' => 'tab',
+                'placement' => 'top',
+            ],
+            [
+                'key' => 'field_aras_localize_enable_hreflang',
+                'label' => 'Enable hreflang',
+                'name' => self::FIELD_ENABLE_HREFLANG,
+                'type' => 'true_false',
+                'ui' => 1,
+                'default_value' => 1,
+            ],
+        ];
     }
 
     public function render() {
@@ -15,6 +35,10 @@ class Hreflang {
         }
 
         if (function_exists('wp_doing_ajax') && wp_doing_ajax()) {
+            return;
+        }
+
+        if (!$this->is_enabled()) {
             return;
         }
 
@@ -40,5 +64,13 @@ class Hreflang {
             echo '<link rel="alternate" hreflang="' . esc_attr($code) . '" href="' . esc_url($url) . '" />' . "\n";
         }
 
+    }
+
+    private function is_enabled() {
+        if (!function_exists('get_field')) {
+            return true;
+        }
+
+        return (bool) get_field(self::FIELD_ENABLE_HREFLANG, 'option');
     }
 }
