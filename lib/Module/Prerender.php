5fb378b9c1fe6e39234878cf9f43755b3070b81c
Mark Fabrizio <fabrizim@owlwatch.com>
2026-08-25 14:28:12 -0400

    fix: update IP address retrieval method in prerender request for improved reliability

--- lib/Module/Prerender.php
+++ lib/Module/Prerender.php
@@ -210,8 +210,13 @@ class Prerender {
             error_log( 'Prerender response body: ' . $body );
             // is there any way to debug the headers sent with the request? I'd like to know the 
             // IP address that we appear to be sending from
-            // what is our ip? lets ask canihazip.com
-            $ip_response = wp_remote_get('https://canihazip.com/s');
+            $ip_response = wp_remote_get(
+                'https://api.ipify.org',
+                [
+                    'timeout' => 10,
+                    'redirection' => 5,
+                ]
+            );
             if (!is_wp_error($ip_response)) {
                 $ip = wp_remote_retrieve_body($ip_response);
                 error_log( 'Prerender request appears to be sending from IP: ' . $ip );
